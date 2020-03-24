6b4440ce29bda253e316e1c6e096cc535fa6439f
006Rain <gengzw@outlook.com>
2020-03-24 23:47:46 +0800

    Upload: Task_info.docx DC_PV_Power_Predict_2018.ipnb

--- main.py
+++ main.py
@@ -9,23 +9,13 @@ if __name__ == "__main__":
     #1. 加载数据
     print( '加载数据=======', datetime.now() )
     data_train, data_test = Load_Data()
-    #print( data_train.shape, data_test.shape )
-    #print( data_train.head(), data_test.head() )
-    #print( data_train.info() ) #可以看出，训练集不存在数据缺失
-    #print( data_test.info() )  #可以看出，测试集不存在数据缺失
-    #print( data_train.describe() ) #可以看出，训练集不存在明显异常值
-    #print( data_test.describe() )  #可以看出，测试集不存在明显异常值
+
     power_mean = np.mean( data_train['实际功率'] )
     power_std = np.std( data_train['实际功率'] )
-    # data = data_train['实际功率'].values
-    # print( 'statistic: ', data[data>20].size ) #num: 15814
-    # print( 'statistic2: ', data[data<0.0].size ) #num: 47837
     
     #2. 特征工程
     print( '处理数据=======', datetime.now() )
     data_train, data_test = Data_Process( data_train, data_test )
-    #print( data_train.head() )
-    #print( data_test.head() )
 
     #测试代码 begin
     #data_train = data_train.iloc[ :10000, : ] #test
